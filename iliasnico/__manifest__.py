d6330248fc54fe95ab0f91441f3fc26d74c5816d
Nicolas Brouwers <nicolas.b.95@hotmail.com>
2022-03-14 12:25:08 +0100

    test

--- iliasnico/__manifest__.py
+++ iliasnico/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name' : 'iliasnico',
-    'version' : '15.0.1',
+    'version' : '15.0.2',
     'summary': 'Invoices & Payments',
     'sequence': 10,
     'description': """
